0242c1b617b0c5ff65807ccbc99bd9704ec94e8d
Faouri <mo.alfauri@gmail.com>
2022-08-10 13:56:39 +0300

    v2

--- m1/ViewController.swift
+++ m1/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import FirebaseCore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupRemoteConfigDefaults()
+        displayNewValues()
+        fetchRemoteConfig()
+
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var rcLabel: UILabel!
+    
+    func setupRemoteConfigDefaults() {
+    let defaultValue = ["label_text": "Hello world!" as NSObject]
+    remoteConfig.setDefaults(defaultValue)
+    }
+    
+   
+    func fetchRemoteConfig(){
+    remoteConfig.fetch(withExpirationDuration: 0) { [unowned self] (status, error) in
+    guard error == nil
+        else { return }
+    print("Got the value from Remote Config!")
+        remoteConfig.activate { (true) in
+            
+        }
+    self.displayNewValues()
+       
+        
+    }
+        
+    }
+            
+            
+    
+   
+func displayNewValues()
+    {
+    let newLabelText = remoteConfig.configValue(forKey: "label_text").stringValue ?? ""
+    rcLabel.text = newLabelText
+    }
+    
+    
+
 
 }
-
